16cdd3c3e28f65b07003b3a15af3b3c8c3eae390
Pablo Pino <pabpinjim@alum.us.es>
2018-11-02 11:15:08 +0100

    Se han añadido las anotaciones de clase y asociaciones de Customer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # D03
 3er entregable de la asignatura DP realizado por el grupo 24
+
+# ANTES DE SUBIR AL REPOSITORIO REMOTO COMPROBAR QUE SÓLO SE SUBEN LOS ARCHIVOS EN src/
--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -3,25 +3,30 @@ package domain;
 
 import java.util.Collection;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 
+@Entity
+@Access(AccessType.PROPERTY)
 public class Customer extends Endorsable {
 
 	//---------Atributos-----------
 
-	private double	score;
-
+	private double					score;
 
 	//----------Relaciones---------
-	private Collection<Note> 		notes;
-	private Collection<Complaint> 	complaints;
+
+	private Collection<Note>		notes;
+	private Collection<Complaint>	complaints;
 	private Collection<FixupTask>	fixupTasks;
-	
-	
-	
+
+
 	//----------Getters y Setters------
 
 	@Range(min = -1, max = 1)
@@ -35,31 +40,34 @@ public class Customer extends Endorsable {
 
 	@Valid
 	@NotNull
+	@OneToMany(mappedBy = "actor")
 	public Collection<Note> getNotes() {
-		return notes;
+		return this.notes;
 	}
 
-	public void setNotes(Collection<Note> notes) {
+	public void setNotes(final Collection<Note> notes) {
 		this.notes = notes;
 	}
 
 	@Valid
 	@NotNull
+	@OneToMany(mappedBy = "customer")
 	public Collection<Complaint> getComplaints() {
-		return complaints;
+		return this.complaints;
 	}
 
-	public void setComplaints(Collection<Complaint> complaints) {
+	public void setComplaints(final Collection<Complaint> complaints) {
 		this.complaints = complaints;
 	}
 
 	@Valid
 	@NotNull
+	@OneToMany(mappedBy = "customer")
 	public Collection<FixupTask> getFixupTasks() {
-		return fixupTasks;
+		return this.fixupTasks;
 	}
 
-	public void setFixupTasks(Collection<FixupTask> fixupTasks) {
+	public void setFixupTasks(final Collection<FixupTask> fixupTasks) {
 		this.fixupTasks = fixupTasks;
 	}
 
