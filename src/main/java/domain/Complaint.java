bc708e2d8b34f646147df0724c1129db1d00639a
Pablo Pino <pabpinjim@alum.us.es>
2018-11-02 10:00:31 +0100

    Se han añadido las anotaciones de clase y asociaciones de Complaint

--- src/main/java/domain/Complaint.java
+++ src/main/java/domain/Complaint.java
@@ -4,7 +4,11 @@ package domain;
 import java.util.Collection;
 import java.util.Date;
 
-
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -12,35 +16,38 @@ import javax.validation.constraints.Past;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
-
+@Entity
+@Access(AccessType.PROPERTY)
 public class Complaint extends Ticketable {
 
 	//----------Atributos-----------
 
-	private Date					moment;
-	private String					description;
-	private Collection<String>		attachments;
+	private Date				moment;
+	private String				description;
+	private Collection<String>	attachments;
 
 	//----------Relaciones---------
 
-	private Report	report;
-	private FixupTask	fixuptask;
-	private Referee referee;
+	private Report				report;
+	private FixupTask			fixuptask;
+	private Referee				referee;
 
 
 	//----------Getters y Setters--------------------
 
 	@Valid
+	@OneToOne(optional = true)
 	public Report getReport() {
 		return this.report;
 	}
 
-	public void setReport(Report report) {
+	public void setReport(final Report report) {
 		this.report = report;
 	}
 
 	@Valid
 	@NotNull
+	@OneToOne(optional = false)
 	public FixupTask getFixuptask() {
 		return this.fixuptask;
 	}
@@ -48,7 +55,6 @@ public class Complaint extends Ticketable {
 	public void setFixuptask(final FixupTask fixuptask) {
 		this.fixuptask = fixuptask;
 	}
-
 
 	@Past
 	@NotNull
@@ -81,11 +87,12 @@ public class Complaint extends Ticketable {
 	}
 
 	@Valid
+	@ManyToOne(optional = true)
 	public Referee getReferee() {
-		return referee;
+		return this.referee;
 	}
 
-	public void setReferee(Referee referee) {
+	public void setReferee(final Referee referee) {
 		this.referee = referee;
 	}
 
