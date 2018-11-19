2382db4bb6ff49c81960380fb2fe3ef6fb01039b
Juan de la Oliva <juandelaoliva1@gmail.com>
2018-11-19 20:40:34 +0100

    Repositorios

---
+++ src/main/java/Repositories/EndorsementRepository.java
@@ -0,0 +1,12 @@
+
+package Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Endorsement;
+
+@Repository
+public interface EndorsementRepository extends JpaRepository<Endorsement, Integer> {
+
+}
