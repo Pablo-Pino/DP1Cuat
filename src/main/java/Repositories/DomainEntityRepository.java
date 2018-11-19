f2f529e33035ec43a588d1e556a5bb795b20852f
Juan de la Oliva <juandelaoliva1@gmail.com>
2018-11-19 20:29:57 +0100

    Repositorios

---
+++ src/main/java/Repositories/DomainEntityRepository.java
@@ -0,0 +1,12 @@
+
+package Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.DomainEntity;
+
+@Repository
+public interface DomainEntityRepository extends JpaRepository<DomainEntity, Integer> {
+
+}
