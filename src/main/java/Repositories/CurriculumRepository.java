398c6636f7d400b3eabad4adb5a1c3abfa071453
Pablo Pino <pabpinjim@alum.us.es>
2018-11-20 11:21:27 +0100

    Merge con master

---
+++ src/main/java/Repositories/CurriculumRepository.java
@@ -0,0 +1,12 @@
+
+package Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Curriculum;
+
+@Repository
+public interface CurriculumRepository extends JpaRepository<Curriculum, Integer> {
+
+}
---
+++ src/main/java/Repositories/EndorserRecordRepository.java
@@ -0,0 +1,12 @@
+
+package Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.EndorserRecord;
+
+@Repository
+public interface EndorserRecordRepository extends JpaRepository<EndorserRecord, Integer> {
+
+}
