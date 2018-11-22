57f05b2bd59e7aafcae4010cbd47e1ee4b7c7bde
alfalatam <alfonsoalarcontamayo27@gmail.com>
2018-11-22 19:07:41 +0100

    Update CreditCardRepository.java

--- src/main/java/repositories/CreditCardRepository.java
+++ src/main/java/repositories/CreditCardRepository.java
@@ -1,49 +1,12 @@
-package services;
 
-import java.util.Collection;
+package repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import repositories.CreditCardRepository;
 import domain.CreditCard;
 
-@Service
-@Transactional
-public class CreditCardService {
-	
-	//Managed Repository
+@Repository
+public interface CreditCardRepository extends JpaRepository<CreditCard, Integer> {
 
-	@Autowired
-	private CreditCardRepository creditCardRepository;
-
-	// Supporting Service
-	
-	
-	// Simple CRUD methods
-	
-	public CreditCard create() {
-		final CreditCard cc = new CreditCard();
-		return cc;
-	}
-	
-	public Collection<CreditCard> findAll() {
-		return this.creditCardRepository.findAll();
-	}
-
-	public CreditCard findOne(final int creditCardId) {
-		return this.creditCardRepository.findOne(creditCardId);
-	}
-	
-	public CreditCard save(final CreditCard cc) {
-		Assert.notNull(cc);
-		return this.creditCardRepository.save(cc);
-	}
-
-	public void delete(final CreditCard cc) {
-		Assert.notNull(cc);
-		this.creditCardRepository.delete(cc);
-	}
 }
