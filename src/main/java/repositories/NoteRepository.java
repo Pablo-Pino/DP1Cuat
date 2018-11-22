e5c69bf59d002ad57fca7a04da94586d9a94cbb2
ElenaMo <molina.elena00@gmail.com>
2018-11-22 18:04:16 +0100

    Update NoteRepository.java

--- src/main/java/repositories/NoteRepository.java
+++ src/main/java/repositories/NoteRepository.java
@@ -1,9 +1,10 @@
+
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import domain.Note;
 
-public interface NoteRepository extends JpaRepository<Note,Integer>{
-hola
+public interface NoteRepository extends JpaRepository<Note, Integer> {
+
 }
