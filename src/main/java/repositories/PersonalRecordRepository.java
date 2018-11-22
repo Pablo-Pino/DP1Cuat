8ce078c52b1b25c195376b45ac6c80c4d08184fc
ElenaMo <36503844+ElenaMo@users.noreply.github.com>
2018-11-22 18:29:26 +0100

    PersonalRecordRepository

---
+++ src/main/java/repositories/PersonalRecordRepository.java
@@ -0,0 +1,11 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import domain.PersonalRecord;
+
+
+public interface PersonalRecordRepository extends JpaRepository<PersonalRecord, Integer>{
+
+
+}
