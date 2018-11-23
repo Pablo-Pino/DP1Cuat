59be3ff5e05157c7f863493249607160a716282c
Juan de la Oliva <juandelaoliva1@gmail.com>
2018-11-23 09:53:40 +0100

    Create EndorserRecordService.java

---
+++ src/main/java/services/EndorserRecordService.java
@@ -0,0 +1,51 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.EndorserRecordRepository;
+import domain.EndorserRecord;
+
+@Transactional
+@Service
+public class EndorserRecordService {
+
+	//---------------Managed Repository---------------------------------------
+
+	@Autowired
+	private EndorserRecordRepository	endorserRecordRepository;
+
+
+	//---------------Supporting Services----------------------------------------
+
+	//------------------Simple CRUD methods----------------------------------
+	public EndorserRecord create() {
+		EndorserRecord res;
+		res = new EndorserRecord();
+		return res;
+	}
+
+	public Collection<EndorserRecord> findAll() {
+		Collection<EndorserRecord> res;
+		res = this.endorserRecordRepository.findAll();
+		return res;
+	}
+
+	public EndorserRecord save(final EndorserRecord endorserRecord) {
+		Assert.notNull(endorserRecord);
+		EndorserRecord res;
+		res = this.endorserRecordRepository.save(endorserRecord);
+		return res;
+	}
+
+	public void delete(final EndorserRecord endorserRecord) {
+		Assert.notNull(endorserRecord);
+		this.endorserRecordRepository.delete(endorserRecord);
+	}
+}
