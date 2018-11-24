0967ff8d65372bf65ff496934bd8ecf36ebb6e4f
ElenaMo <36503844+ElenaMo@users.noreply.github.com>
2018-11-24 12:24:57 +0100

    @Repository olvidado

--- src/main/java/repositories/TutorialRepository.java
+++ src/main/java/repositories/TutorialRepository.java
@@ -4,7 +4,7 @@ package repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import domain.Tutorial;
-
+@Repository
 public interface TutorialRepository extends JpaRepository<Tutorial, Integer> {
 
 }
