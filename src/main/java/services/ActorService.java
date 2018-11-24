a8a44b3b25155a1629dc757690f8a77df6d70ca9
alfalatam <alfonsoalarcontamayo27@gmail.com>
2018-11-24 15:51:47 +0100

    Update ActorService.java

--- src/main/java/services/ActorService.java
+++ src/main/java/services/ActorService.java
@@ -2,12 +2,12 @@
 package services;
 
 import java.util.Collection;
-import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import repositories.ActorRepository;
 import security.LoginService;
@@ -16,60 +16,60 @@ import domain.Actor;
 
 @Service
 @Transactional
-public class ActorService extends GenericService<Actor, ActorRepository> implements ServiceI<Actor> {
+public class ActorService {
 
-	// Repositories
+	//--------------------Repositories--------------------------
 
 	@Autowired
-	private ActorRepository	repository;
+	private ActorRepository	actorRepository;
+
+	//--------------------Services------------------------------
+
+	@Autowired
+	private FolderService	folderService;
 
 
-	// Services
+	// ------------------CRUD methods-----------------------------
 
-	//@Autowired
-	//private FolderService	folderService;
+	public Actor findOne(final int actorId) {
+		Assert.isTrue(actorId != 0);
+		Actor result;
+		result = this.actorRepository.findOne(actorId);
+		Assert.notNull(result);
 
-	// CRUD methods
-
-	@Override
-	public Actor findOne(final Integer id) {
-		return super.findOne(id);
+		return result;
 	}
 
-	@Override
-	public List<Actor> findAll(final Collection<Integer> ids) {
-		return super.findAll(ids);
+	public Collection<Actor> findAll() {
+		Collection<Actor> result;
+		result = this.actorRepository.findAll();
+		Assert.notNull(result);
+
+		return result;
 	}
 
-	@Override
-	public List<Actor> findAll() {
-		return super.findAll();
+	public Actor save(final Actor actor) {
+		Assert.notNull(actor);
+		final Actor result = this.actorRepository.save(actor);
+		return result;
 	}
 
-	// Other methods
+	public void delete(final Actor actor) {
+		Assert.notNull(actor);
+		Assert.isTrue(actor.getId() != 0);
+		Assert.isTrue(this.actorRepository.exists(actor.getId()));
+
+		this.actorRepository.delete(actor);
+	}
+	// -------------------------Other methods-------------------------
 
 	public Actor findOneByUserAccount(final UserAccount userAccount) {
-		return this.repository.findOneByUserAccount(userAccount.getId());
+		return this.actorRepository.findOneByUserAccount(userAccount.getId());
 	}
 
 	public Actor findPrincipal() {
 		final UserAccount userAccount = LoginService.getPrincipal();
-		return this.repository.findOneByUserAccount(userAccount.getId());
-	}
-
-	@Override
-	public Actor create() {
-		throw new IllegalArgumentException("Unallowed method");
-	}
-
-	@Override
-	public Actor save(final Actor object) {
-		throw new IllegalArgumentException("Unallowed method");
-	}
-
-	@Override
-	public void delete(final Actor object) {
-		throw new IllegalArgumentException("Unallowed method");
+		return this.actorRepository.findOneByUserAccount(userAccount.getId());
 	}
 
 }
