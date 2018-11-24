ed9de78c66da8efb422fb8c0c09ae4d9337bcfeb
ElenaMo <36503844+ElenaMo@users.noreply.github.com>
2018-11-24 12:22:09 +0100

    @Repository que se me habia olvidao

--- src/main/java/repositories/WorkPlanRepository.java
+++ src/main/java/repositories/WorkPlanRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import domain.WorkPlan;
 
+@Repository
 public interface WorkPlanRepository extends JpaRepository<WorkPlan, Integer> {
 
 }
