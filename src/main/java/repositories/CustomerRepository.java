110c7fd9d31f201e56b804bee44b9ad301f46fdf
Juan de la Oliva <juandelaoliva1@gmail.com>
2018-11-25 18:12:30 +0100

    Corregido "sevrices"

--- src/main/java/repositories/CustomerRepository.java
+++ src/main/java/repositories/CustomerRepository.java
@@ -1,18 +1,12 @@
 
 package repositories;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-	@Query("select c from Customer c join c.fixupTasks f group by c.id order by sum(f.complaints.size) desc")
-	Collection<Customer> getTop3CustomerWithMoreComplaints();
 
 }
