0e483fa3bbe7fcb49847115c08a6034c5cac6e11
Juan de la Oliva <juandelaoliva1@gmail.com>
2018-11-25 19:01:15 +0100

    Customer avance

--- src/test/java/services/CustomerServiceTest.java
+++ src/test/java/services/CustomerServiceTest.java
@@ -3,11 +3,15 @@ package services;
 
 import javax.transaction.Transactional;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
+import domain.Customer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -16,4 +20,17 @@ import utilities.AbstractTest;
 @Transactional
 public class CustomerServiceTest extends AbstractTest {
 
+	//Service under test ------------------------------------------
+
+	@Autowired
+	private CustomerService	customerService;
+
+
+	@Test
+	public void testFindOne() {
+		Customer customer;
+
+		customer = this.customerService.findOne(super.getEntityId("customer1"));
+		Assert.notNull(customer);
+	}
 }
