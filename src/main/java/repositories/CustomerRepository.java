ecacd1aa051268ba7aa3e608b7e8d52bdd98671f
Pablo Pino <pabpinjim@alum.us.es>
2018-11-25 19:43:44 +0100

    Añadida query faltante en CustomerRepository

--- src/main/java/repositories/CustomerRepository.java
+++ src/main/java/repositories/CustomerRepository.java
@@ -15,4 +15,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 	@Query("select c from Customer c join c.fixupTasks f group by c.id order by sum(f.complaints.size) desc")
 	Collection<Customer> getTop3CustomerWithMoreComplaints();
 
+	@Query("select c from Customer c join c.fixupTasks t group by c having   c.fixupTasks.size >= ( select avg(c1.fixupTasks.size) from Customer c1) order by t.applications.size")
+	Collection<Customer> ListOfMorePublishingCustomers();
+
 }
