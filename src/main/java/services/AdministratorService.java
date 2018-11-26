34fed3d36c1a4f75a906df95f85da941fef0f3d6
alfalatam <alfonsoalarcontamayo27@gmail.com>
2018-11-26 13:57:21 +0100

    App y Admin Service y Test
    test ya funcionales

--- src/main/java/services/AdministratorService.java
+++ src/main/java/services/AdministratorService.java
@@ -1,6 +1,7 @@
 
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,18 +10,31 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.AdministratorRepository;
+import security.Authority;
 import security.UserAccount;
 import domain.Administrator;
+import domain.Folder;
+import domain.Message;
+import domain.SocialProfile;
 
 @Service
 @Transactional
 public class AdministratorService {
 
-	// Supporting Services
+	//--------------Managed repository---------------------------
 
-	// ------------------------------------------------------------
 	@Autowired
 	private AdministratorRepository	administratorRepository;
+
+	//-------------- Supporting Services-----------------------
+
+	//Actor service no le haria falta?
+
+	@Autowired
+	private FolderService			folderService;
+
+	@Autowired
+	private ServiceUtils			serviceUtils;
 
 
 	// --------------------------Constructor-----------------------
@@ -32,27 +46,61 @@ public class AdministratorService {
 	// --------------------CRUD methods----------------------------
 
 	public Administrator create() {
-		final Administrator f = new Administrator();
-		f.setUserAccount(new UserAccount());
-		return f;
+		final Administrator result = new Administrator();
+		result.setBanned(false);
+		result.setSuspicious(false);
+		//----- Las listas que tiene que tener se las pongo vacias-------------
+		result.setSocialProfiles(new ArrayList<SocialProfile>());
+		result.setFolders(new ArrayList<Folder>());
+		result.setReceivedMessages(new ArrayList<Message>());
+		result.setSendedMessages(new ArrayList<Message>());
+		result.setUserAccount(new UserAccount());
+		return result;
+
 	}
 
-	public Administrator save(final Administrator administrator) {
-		Assert.notNull(administrator);
-		return this.administratorRepository.save(administrator);
+	public Administrator save(final Administrator object) {
+		final Administrator admin = this.checkObjectSave(object);
+		if (object.getId() == 0) {
+			object.setBanned(false);
+			object.setFolders(this.folderService.createSystemFolders(object));
+			object.setReceivedMessages(new ArrayList<Message>());
+			object.setSendedMessages(new ArrayList<Message>());
+			object.setSocialProfiles(new ArrayList<SocialProfile>());
+			object.setSuspicious(false);
+			this.checkPermisionActor(null, new String[] {
+				Authority.ADMIN
+			});
+		} else {
+			object.setBanned(admin.getBanned());
+			object.setFolders(admin.getFolders());
+			object.setReceivedMessages(admin.getReceivedMessages());
+			object.setSendedMessages(admin.getSendedMessages());
+			object.setSocialProfiles(admin.getSocialProfiles());
+			object.setSuspicious(admin.getSuspicious());
+			object.setUserAccount(admin.getUserAccount());
+			this.checkPermisionActor(admin, new String[] {
+				Authority.REFEREE
+			});
+		}
+		final Administrator res = this.administratorRepository.save(object);
+		return res;
 	}
+
+	//----------------------------
 
 	public Administrator findOne(final int administratorId) {
 		return this.administratorRepository.findOne(administratorId);
 	}
 
 	public Collection<Administrator> findAll() {
-		Collection<Administrator> a;
+		Collection<Administrator> res;
+		res = this.administratorRepository.findAll();
+		Assert.notNull(res);
 
-		a = this.administratorRepository.findAll();
-		Assert.notNull(a);
-
-		return a;
+		return res;
 	}
+
+	// -------------------------Other business methods ------------------------------
 
 }
---
+++ src/test/java/services/AdministratorServiceTest.java
@@ -0,0 +1,63 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.Administrator;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/datasource.xml", "classpath:spring/config/packages.xml"
+})
+@Transactional
+public class AdministratorServiceTest extends AbstractTest {
+
+	//Service under test ------------------------------------------
+
+	@Autowired
+	private AdministratorService	administratorService;
+
+
+	@Test
+	public void testCreate() {
+		final Administrator a = this.administratorService.create();
+		Assert.notNull(a);
+	}
+	@Test
+	public void testFindOneCorrecto() {
+		Administrator admin;
+
+		final int idBusqueda = super.getEntityId("administrator1");
+		admin = this.administratorService.findOne(idBusqueda);
+		Assert.notNull(admin);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testFindOneIncorrecto() {
+		Administrator admin;
+
+		final int idBusqueda = super.getEntityId("abelardo");
+		admin = this.administratorService.findOne(idBusqueda);
+		Assert.isNull(admin);
+	}
+
+	@Test
+	public void testFindAll() {
+		Collection<Administrator> admins;
+
+		admins = this.administratorService.findAll();
+		Assert.notNull(admins);
+		Assert.notEmpty(admins); //porque sabemos que hemos creado algunos con el populate
+	}
+
+}
