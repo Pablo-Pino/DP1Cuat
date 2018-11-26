845d90e3bddf25dc31a344a30ca711ce45f4e1bc
Juan de la Oliva <juandelaoliva1@gmail.com>
2018-11-26 13:55:41 +0100

    Create EndorsableService.java

---
+++ src/main/java/services/EndorsableService.java
@@ -0,0 +1,51 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.EndorsableRepository;
+import domain.Endorsable;
+
+@Service
+@Transactional
+public class EndorsableService {
+
+	//--------------------Managed Repositories-------------------------
+
+	@Autowired
+	private EndorsableRepository	endorsableRepository;
+
+
+	// Supporting services ----------------------------------------------------
+
+	// Constructors -----------------------------------------------------------
+
+	// Simple CRUD methods ----------------------------------------------------
+	public Endorsable findOne(final int endorsableId) {
+		Assert.isTrue(endorsableId != 0);
+		Endorsable res;
+		res = this.endorsableRepository.findOne(endorsableId);
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Collection<Endorsable> findAll() {
+		Collection<Endorsable> res;
+		res = this.endorsableRepository.findAll();
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Endorsable save(final Endorsable endorsable) {
+		Endorsable res;
+		res = this.endorsableRepository.save(endorsable);
+		return res;
+
+	}
+}
