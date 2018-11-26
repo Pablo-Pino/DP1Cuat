006751496418df022273f259c32c2a02a2726a97
Juan de la Oliva <juandelaoliva1@gmail.com>
2018-11-26 12:24:05 +0100

    Customer service y test(pobre)

--- src/main/java/services/CustomerService.java
+++ src/main/java/services/CustomerService.java
@@ -31,9 +31,6 @@ public class CustomerService {
 	// Supporting services ----------------------------------------------------
 
 	@Autowired
-	private ActorService		actorService;
-
-	@Autowired
 	private FolderService		folderService;
 
 	@Autowired
@@ -54,43 +51,18 @@ public class CustomerService {
 	public Customer create() {
 		Customer result;
 		result = new Customer();
+		//los atributos que no pueden estar vacíos
 		result.setBanned(false);
 		result.setSuspicious(false);
 		result.setFolders(new ArrayList<Folder>());
 		result.setReceivedMessages(new ArrayList<Message>());
 		result.setSendedMessages(new ArrayList<Message>());
 		result.setSocialProfiles(new ArrayList<SocialProfile>());
+		//establezco ya su tipo de userAccount porque no va a cambiar
 		result.setUserAccount(this.userAccountService.create("CUSTOMER"));
 		return result;
 	}
-	//	public Customer create() {
-	//		UserAccount userAccount;
-	//		Authority authority;
-	//		final Collection<Authority> authorities;
-	//		Collection<Folder> folders;
-	//		Customer result;
-	//
-	//		folders = new ArrayList<Folder>();
-	//
-	//		authority = new Authority();
-	//		authority.setAuthority(Authority.CUSTOMER);
-	//
-	//		authorities = new ArrayList<Authority>();
-	//		authorities.add(authority);
-	//
-	//		userAccount = new UserAccount();
-	//		userAccount.setAuthorities(authorities);
-	//
-	//		result = new Customer();
-	//		result.setUserAccount(userAccount);
-	//		Collection<Fix>
-	//		result.setFixupTasks(fixupTasks)
-	//
-	//		folders = this.folderService.createSystemFolders(result);
-	//		result.setFolders(folders);
-	//
-	//		return result;
-	//	}
+
 	public Customer findOne(final int customerId) {
 		Customer res;
 
@@ -111,12 +83,20 @@ public class CustomerService {
 	}
 
 	public Customer save(final Customer customer) {
+		//comprobamos que el customer que nos pasan no sea nulo
 		Assert.notNull(customer);
+
+		//comprobamos que su id no sea negativa por motivos de seguridad
 		this.serviceUtils.checkIdSave(customer);
+
+		//este customer será el que está en la base de datos para usarlo si estamos ante un customer que ya existe
 		Customer customerBD;
 		Assert.isTrue(customer.getId() > 0);
+
+		//cogemos el customer de la base de datos
 		customerBD = this.customerRepository.findOne(customer.getId());
 
+		//Si el customer que estamos guardando es nuevo (no está en la base de datos) le ponemos todos sus atributos vacíos
 		if (customer.getId() == 0) {
 			customer.setBanned(false);
 			customer.setFixupTasks(new ArrayList<FixupTask>());
@@ -125,8 +105,10 @@ public class CustomerService {
 			customer.setSendedMessages(new ArrayList<Message>());
 			customer.setSocialProfiles(new ArrayList<SocialProfile>());
 			customer.setSuspicious(false);
-			this.serviceUtils.checkAuthority("ADMIN");
-			this.serviceUtils.checkActor(customer);
+
+			//comprobamos que ningún actor resté autenticado (ya que ningun actor puede crear los customers)
+			this.serviceUtils.checkNoActor();
+
 		} else {
 			customer.setBanned(customerBD.getBanned());
 			customer.setFixupTasks(customerBD.getFixupTasks());
@@ -136,11 +118,15 @@ public class CustomerService {
 			customer.setSocialProfiles(customerBD.getSocialProfiles());
 			customer.setSuspicious(customerBD.getSuspicious());
 			customer.setUserAccount(customerBD.getUserAccount());
+
+			//Comprobamos que el actor sea un Customer
 			this.serviceUtils.checkAuthority("CUSTOMER");
+			//esto es para ver si el actor que está logueado es el mismo que se está editando
 			this.serviceUtils.checkActor(customer);
 
 		}
 		Customer res;
+		//le meto al resultado final el customer que he ido modificando anteriormente
 		res = this.customerRepository.save(customer);
 		return res;
 	}
