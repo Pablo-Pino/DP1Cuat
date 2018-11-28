c5c17d5c17f2935bbe01a299c3287803e09adce1
ElenaMo <molina.elena00@gmail.com>
2018-11-28 19:19:30 +0100

    Cambios

--- src/test/java/services/ActorServiceTest.java
+++ src/test/java/services/ActorServiceTest.java
@@ -1,6 +1,7 @@
 
 package services;
 
+import java.util.Collection;
 import java.util.Map;
 
 import javax.transaction.Transactional;
@@ -10,6 +11,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import domain.Actor;
 
 import utilities.AbstractTest;
 
@@ -31,6 +35,31 @@ public class ActorServiceTest extends AbstractTest {
 		this.authenticate("customer1");
 		final Map<String, Double> result = this.actorService.fixupTasksStats();
 		System.out.println("Las variables de FixupTask para este usuario son: " + result);
+
+	}
+	@Test
+	public void testgetSuspicious() {
+		this.authenticate("administrator1");
+
+		Collection<Actor> suspicious;
+		suspicious = this.actorService.suspiciousActors();
+		Assert.notEmpty(suspicious);
+		System.out.println("Lista de actores sospechosos: " + suspicious);
+	
+	}
+	
+	@Test
+	public void testBanUnban() {
+		//this.authenticate("super");
+		int actorId = this.getEntityId("administrator1");
+		Actor actor;
+		actor = this.actorService.findOne(actorId);
+		this.actorService.banActor(actor);
+		Assert.notNull(actor);
+		Assert.isTrue(actor.getBanned());
+		this.actorService.unbanActor(actor);
+		Assert.notNull(actor);
+		Assert.isTrue(!(actor.getBanned()));
 
 	}
 
