375decd2206a80733202f25cc4411c3a4ccc88fc
Pablo Pino <pabpinjim@alum.us.es>
2018-11-28 19:41:49 +0100

    Añadidos varios test CRUD a PhaseService

--- src/test/java/services/PhaseServiceTest.java
+++ src/test/java/services/PhaseServiceTest.java
@@ -1,11 +1,13 @@
 
 package services;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
 import javax.transaction.Transactional;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
+import domain.FixupTask;
 import domain.Phase;
 import domain.WorkPlan;
 
@@ -26,9 +29,11 @@ public class PhaseServiceTest extends AbstractTest {
 	// Services
 
 	@Autowired
-	private PhaseService	phaseService;
+	private PhaseService		phaseService;
 	@Autowired
-	private WorkPlanService	workPlanService;
+	private WorkPlanService		workPlanService;
+	@Autowired
+	private FixupTaskService	fixupTaskService;
 
 
 	// Tests
@@ -63,18 +68,18 @@ public class PhaseServiceTest extends AbstractTest {
 		super.checkExceptions(expected, caught);
 	}
 
-	public void findByWorkPlanPhase(final Integer workPlanId, final Class<?> expected) {
+	public void findByWorkPlanPhase(final WorkPlan workPlan, final Class<?> expected) {
 		Class<?> caught = null;
 		try {
-			final WorkPlan workPlan = this.workPlanService.findOne(workPlanId);
 			final Collection<Phase> phases = this.phaseService.findAll(workPlan);
-			Assert.isTrue(workPlan.getPhases().equals(phases));
+			Assert.isTrue(workPlan.getPhases().size() == phases.size());
+			for (final Phase p : phases)
+				Assert.isTrue(workPlan.getPhases().contains(p));
 		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		}
 		super.checkExceptions(expected, caught);
 	}
-
 	public void createPhase(final String username, final Integer workPlanId, final Class<?> expected) {
 		Class<?> caught = null;
 		try {
@@ -138,6 +143,84 @@ public class PhaseServiceTest extends AbstractTest {
 			caught = oops.getClass();
 		}
 		this.checkExceptions(expected, caught);
+	}
+
+	@Test
+	public void testFindOnePhase() {
+		this.findOnePhase(super.getEntityId("phase1"), null);
+	}
+
+	@Test
+	public void testFindOnePhaseNullId() {
+		this.findOnePhase(null, IllegalArgumentException.class);
+	}
+
+	@Test
+	public void testFindAllByIdsPhase() {
+		this.findAllPhase(Arrays.asList(new Integer[] {
+			super.getEntityId("phase1"), super.getEntityId("phase2")
+		}), null);
+	}
+
+	@Test
+	public void testFindAllByIdsPhaseNullIds() {
+		this.findAllPhase(null, IllegalArgumentException.class);
+	}
+
+	@Test
+	public void testFindAllPhase() {
+		this.findAllPhase(null);
+	}
+
+	@Test
+	public void testFindAllByWorkPlanPhase() {
+		final WorkPlan workPlan = this.workPlanService.findOne(super.getEntityId("workPlan1"));
+		this.findByWorkPlanPhase(workPlan, null);
+	}
+
+	// En este caso se genera un NullPointerException debido a que al buscar el WorkPlan, se llama al
+	// metodo getId() de WorkPlan, y al ser este nulo se provoca un NullPointerException
+	@Test
+	public void testFindAllByWorkPlanPhaseNullWorkPlan() {
+		this.findByWorkPlanPhase(null, NullPointerException.class);
+	}
+
+	@Test
+	public void testSavePhase() {
+		final Integer workPlanId = super.getEntityId("workPlan1");
+		final FixupTask fixupTask = this.fixupTaskService.findOne(super.getEntityId("fixupTask1"));
+		this.savePhase("handywoker1", "A phase", "A nice phase", new Date(fixupTask.getStart().getTime() + 1000), new Date(fixupTask.getEnd().getTime() - 1000), null, workPlanId, null);
+	}
+
+	@Test
+	public void testSavePhaseUnauthenticated() {
+		final Integer workPlanId = super.getEntityId("workPlan1");
+		final FixupTask fixupTask = this.fixupTaskService.findOne(super.getEntityId("fixupTask1"));
+		this.savePhase(null, "A phase", "A nice phase", new Date(fixupTask.getStart().getTime() + 1000), new Date(fixupTask.getEnd().getTime() - 1000), null, workPlanId, IllegalArgumentException.class);
+	}
+
+	@Test
+	public void testUpdatePhase() {
+		final Integer workPlanId = super.getEntityId("workPlan1");
+		final FixupTask fixupTask = this.fixupTaskService.findOne(super.getEntityId("fixupTask1"));
+		this.savePhase("handywoker1", "A phase", "A nice phase", new Date(fixupTask.getStart().getTime() + 1000), new Date(fixupTask.getEnd().getTime() - 1000), super.getEntityId("phase1"), workPlanId, null);
+	}
+
+	@Test
+	public void testUpdatePhaseUnauthenticated() {
+		final Integer workPlanId = super.getEntityId("workPlan1");
+		final FixupTask fixupTask = this.fixupTaskService.findOne(super.getEntityId("fixupTask1"));
+		this.savePhase(null, "A phase", "A nice phase", new Date(fixupTask.getStart().getTime() + 1000), new Date(fixupTask.getEnd().getTime() - 1000), super.getEntityId("phase1"), workPlanId, IllegalArgumentException.class);
+	}
+
+	@Test
+	public void testDeletePhase() {
+		this.deletePhase("handywoker1", super.getEntityId("phase1"), null);
+	}
+
+	@Test
+	public void testDeletePhaseUnauthenticated() {
+		this.deletePhase(null, super.getEntityId("phase2"), IllegalArgumentException.class);
 	}
 
 }
