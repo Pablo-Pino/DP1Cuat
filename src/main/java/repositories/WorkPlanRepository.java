27641c84552c7465a5a5e7552815e97755881609
Pablo Pino <pabpinjim@alum.us.es>
2018-12-02 23:39:37 +0100

    Añadida query que busca WorkPlans según el HandyWorker

--- src/main/java/repositories/WorkPlanRepository.java
+++ src/main/java/repositories/WorkPlanRepository.java
@@ -1,7 +1,10 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.WorkPlan;
@@ -9,4 +12,7 @@ import domain.WorkPlan;
 @Repository
 public interface WorkPlanRepository extends JpaRepository<WorkPlan, Integer> {
 
+	@Query("select w from WorkPlan w where w.handyWorker.id = ?1")
+	Collection<WorkPlan> findWorkPlanByHandyWorker(int handyWorkerId);
+	
 }
