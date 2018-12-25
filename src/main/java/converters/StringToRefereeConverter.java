e9bcfaebd24c51be1ae1231110fe9a8849a2422b
Pablo Pino <pabpinjim@alum.us.es>
2018-12-25 13:26:02 +0100

    Se han añadido los converters de Phase, Referee y Report

---
+++ src/main/java/converters/StringToRefereeConverter.java
@@ -0,0 +1,39 @@
+
+package converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import repositories.RefereeRepository;
+import domain.Referee;
+
+@Component
+@Transactional
+public class StringToRefereeConverter implements Converter<String, Referee> {
+
+	@Autowired
+	private RefereeRepository	repository;
+
+
+	@Override
+	public Referee convert(final String s) {
+		Referee res;
+		int id;
+
+		try {
+			if (!StringUtils.isEmpty(s))
+				res = null;
+			else {
+				id = Integer.valueOf(s);
+				res = this.repository.findOne(id);
+			}
+		} catch (final Throwable t) {
+			throw new IllegalArgumentException(t);
+		}
+		return res;
+	}
+
+}
---
+++ src/main/java/converters/StringToReportConverter.java
@@ -0,0 +1,39 @@
+
+package converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import repositories.ReportRepository;
+import domain.Report;
+
+@Component
+@Transactional
+public class StringToReportConverter implements Converter<String, Report> {
+
+	@Autowired
+	private ReportRepository	repository;
+
+
+	@Override
+	public Report convert(final String s) {
+		Report res;
+		int id;
+
+		try {
+			if (!StringUtils.isEmpty(s))
+				res = null;
+			else {
+				id = Integer.valueOf(s);
+				res = this.repository.findOne(id);
+			}
+		} catch (final Throwable t) {
+			throw new IllegalArgumentException(t);
+		}
+		return res;
+	}
+
+}
