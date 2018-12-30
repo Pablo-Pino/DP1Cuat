6b02234f21dddd7ab6f51d2d4558e2f180bdd301
Pablo Pino <pabpinjim@alum.us.es>
2018-12-30 19:38:30 +0100

    Corregidos Customer y FixupTask
    Se ha añadido el atributo fixupTasks a Customer.
    
    Se han añadido los atributos applications y compplaints a Customer.
    
    Se han añadido estos atributos debido a que son necesarios para las
    siguientes queries:
    
    	The minimum, the maximum, the average, and the standard deviation
    of the number of complaints per fix-up task.
    
    	The average, the minimum, the maximum, and the standard deviation
    of the number of fix-up tasks per user.
    
    	The average, the minimum, the maximum, and the standard deviation
    of the number of applications per fix-up task.

--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -1,10 +1,14 @@
 
 package domain;
 
+import java.util.Collection;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -13,6 +17,19 @@ public class Customer extends Endorsable {
 	//---------Atributos-----------
 
 	//----------Relaciones---------
+
+	private Collection<FixupTask>	fixupTasks;
+
+
+	@NotNull
+	@Valid
+	@OneToMany(mappedBy = "customer")
+	public Collection<FixupTask> getFixupTasks() {
+		return this.fixupTasks;
+	}
+	public void setFixupTasks(final Collection<FixupTask> fixupTasks) {
+		this.fixupTasks = fixupTasks;
+	}
 
 	//----------Getters y Setters------
 
