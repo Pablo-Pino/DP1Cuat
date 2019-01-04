704eb5514422538e13d893bf15ad3c51c8f79ba5
alfalatam <alfonsoalarcontamayo27@gmail.com>
2019-01-04 18:17:52 +0100

    subida

--- src/main/java/controllers/ComplaintController.java
+++ src/main/java/controllers/ComplaintController.java
@@ -1,6 +1,40 @@
+
 package controllers;
 
+import java.util.Collection;
 
-public class ComplaintController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.ComplaintService;
+import domain.Complaint;
+
+@Controller
+@RequestMapping("/complaint")
+public class ComplaintController extends AbstractController {
+
+	//-----------------Services-------------------------
+	@Autowired
+	private ComplaintService	complaintService;
+
+
+	//-----------------List----------------------------
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView result;
+		Collection<Complaint> res;
+
+		res = this.complaintService.findAll();
+
+		result = new ModelAndView("complaint/list");
+		result.addObject("requestURI", "complaint/list.do");
+		result.addObject("complaints", res);
+
+		return result;
+	}
 
 }
